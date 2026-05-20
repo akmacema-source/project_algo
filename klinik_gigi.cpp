e6f9dacf78e863244a17e220c779474d841c20c7
akmacema-source <akmacema@gmail.com>
2026-05-20 22:15:21 +0700

    Tambah struct pasien dengan field data dan pointer doubly linked list

--- klinik_gigi.cpp
+++ klinik_gigi.cpp
@@ -3,3 +3,16 @@
 #include <cstring>
 #include <cstdlib>
 using namespace std;
+
+struct pasien
+{
+    char nama[50];
+    char keluhan[50];
+    char tindakan[50];
+    char jadwalTemu[30];
+    char tanggalTemu[20];
+    int  NIK;
+    int  biaya;
+    pasien *kanan;
+    pasien *kiri;
+};
